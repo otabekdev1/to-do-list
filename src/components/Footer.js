5249a490cd17fe5a7ce8982b037e7a487c9756f3
otabekdev1 <forthepubghero@gmail.com>
2022-12-03 21:19:23 +0500

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <h2 className="footer-logo" >
-      Made by <a href="https://t.me/isomov_otabek">Otabek</a>
+    <h2 className="footer-logo">
+      Made by{" "}
+      <a href="https://t.me/isomov_otabek" target={"_blank"}>
+        Otabek
+      </a>
     </h2>
   );
 }
